fd10a5203af5ad280ef919cd105e119027e741a6
Cry <943081641@qq.com>
2019-02-20 16:17:59 +0800

    SliderControl
    SliderControl 控件上传

---
+++ SliderControl/SliderControl/SliderControl/SliderControlCell.h
@@ -0,0 +1,15 @@
+//
+//  SliderControlCell.h
+//  SliderControl
+//
+//  Created by Cry on 2018/9/11.
+//  Copyright © 2018年 foxhis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SliderControlCell : UICollectionViewCell
+@property (weak, nonatomic) IBOutlet UILabel *title;
+@property (weak, nonatomic) IBOutlet UIView *bottomView;
+
+@end
